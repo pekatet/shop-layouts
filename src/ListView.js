6b71cef0bd81fe9b46f351b503c627ea0e581365
pekatet <32317289+pekatet@users.noreply.github.com>
2022-02-23 21:46:05 +0700

    add prop-types for list

--- src/ListView.js
+++ src/ListView.js
@@ -2,6 +2,7 @@ import './Store.css';
 import React from 'react';
 import {uid} from 'react-uid';
 import ShopItem from './ShopItem'
+import PropTypes from 'prop-types'
 
 function ListView({items}){
   return(
@@ -13,6 +14,15 @@ function ListView({items}){
       })}
     </div>
   )
+}
+
+ListView.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired
+  }).isRequired)
 }
 
 export default ListView;

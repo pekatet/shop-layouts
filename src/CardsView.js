c5badf72cd0a18b74aceac4f8b8b744f61579644
pekatet <32317289+pekatet@users.noreply.github.com>
2022-02-23 21:44:59 +0700

    add prop-types for cards

--- src/CardsView.js
+++ src/CardsView.js
@@ -2,6 +2,7 @@ import './Store.css';
 import React from 'react'
 import {uid} from 'react-uid'
 import ShopCard from './ShopCard'
+import PropTypes from 'prop-types'
 
 function CardsView({cards}){
   return(
@@ -13,6 +14,15 @@ function CardsView({cards}){
       })}
     </div>
   )
+}
+
+CardsView.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired
+  }).isRequired)
 }
 
 export default CardsView
